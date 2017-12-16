f06fa4fbb373abebfa8532b7e9194159ec7e3e70
nowido <nowido@mail.ru>
2017-12-16 20:26:52 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
+import { setInterval } from 'timers';
+
 const http = require('http');
+
+const PERIOD = 10 * 1000;
+const pingTarget = 'fluidsync2.herokuapp.com';
+
+const pingOptions = 
+{
+    hostname: pingTarget//,
+    //port: process.env.PORT
+};
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
@@ -6,6 +17,16 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.end('Hello World\n');
 });
+
+setInterval(() => {
+
+    const pingRequest = http.request(pingOptions, (res) => {
+        console.log('!!');
+    });
+    
+    pingRequest.end();
+
+}, PERIOD);
 
 server.listen(process.env.PORT, () => {
   console.log('Server running...');
