f17069bf160004f818279253a9522698a8d3de9a
nowido <nowido@mail.ru>
2017-12-24 15:06:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const httpServer = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.end('Hello World\n');
+  res.end('OK');
 });
 
 const io = new socketServer(httpServer);
